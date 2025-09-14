b52e1a797e26e7e418aa7c5d7be368e269c31e75
B-Matthieu <66334672+B-Matthieu@users.noreply.github.com>
2025-09-14 17:34:38 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Twitch Extension Helper
-Twitch.ext.onAuthorized((auth) => {
-  document.getElementById("message").innerText =
-    "Hello, " + auth.channelId + "!";
-});
-
-Twitch.ext.onContext((context) => {
-  console.log("Extension context:", context);
-});
